5b2d3cc91ad1b6422cb352969806d884ce4ae0f2
Osindeeis <mstiteli4@yandex.ru>
2022-06-13 19:46:09 +0400

    loading and receiving data from localStorage

--- js/scritp.js
+++ js/scritp.js
@@ -1,71 +1,77 @@
 "use strict"
 
 const toDo = document.querySelector(".container")
-const openForm= document.querySelector(".Add")
+const openForm = document.querySelector(".Add")
 const addCard = document.querySelector(".AddCard")
 let form = document.querySelector(".pop-up")
 let Inputtitle = document.getElementById("title");
-let InputText= document.getElementById("description");
-let cards = [];
-openForm.addEventListener("click",function(){
-    form.style.opacity=1;
-    form.style.visibility="visible";
+let InputText = document.getElementById("description");
+let cards;
+
+
+if(localStorage.card){
+    cards = JSON.parse(localStorage.getItem("card"))
+}
+else{
+    cards=[]
+}
+
+
+
+openForm.addEventListener("click", function () {
+    form.style.opacity = 1;
+    form.style.visibility = "visible";
 })
 
+function Card(title, description) {
+    this.title = title;
+    this.description = description;
+    this.complete = false;
+}
 
+const CreateCard = (card, index) => {
+    return `
+    <div class="card">
+    <h3 class="title">${card.title}</h3>
+    <hr/>
+    <p class="scroll">${card.description}</p>
+    <label>Приоритет:
+        <select>
+            <option>Низкий</option>
+            <option>Средний</option>
+            <option>Высокий</option>
+        </select>
+    <label>
 
-function AddCard(title,description){
-    const card={
-        id:`${Math.random()}`,
-        title,
-        description,
-        cardStatus:false
-    };
-    cards.push(card);
+    <div class="card__row">
+        <button class="remove">Удалить</button>
+        <button class="change">Изменить</button>
+    </div>
+    
+    </div>
+    `
+}
+
+const Show = ()=>{
+    toDo.innerHTML=""
+    if(cards.length>0){
+        cards.forEach((item,index) => {
+            toDo.innerHTML += CreateCard(item,index); 
+        });
+    }
+}
+
+Show();
+
+const updateLocal = ()=>{
+    localStorage.setItem("card",JSON.stringify(cards))
 }
 
 addCard.addEventListener("click",function(){
-    AddCard(Inputtitle.value, InputText.value)
-    form.style.opacity=0;
-    form.style.visibility="hidden";
-    showOnDisplay();
+    cards.push(new Card(Inputtitle.value,InputText.value))
+    updateLocal();
+    Show();
+    form.style.opacity = 0;
+    form.style.visibility = "hidden";
 })
-
-function showOnDisplay(){
-let html='';
-
-    cards.forEach(card => {
-        if(card.cardStatus){
-            return;
-        }
-        html += `
-            <div class="card">
-                <h3 class="title">${card.title}</h3>
-                <hr/>
-                <p class="scroll">${card.description}</p>
-                <label>Приоритет:
-                    <select>
-                        <option>Низкий</option>
-                        <option>Средний</option>
-                        <option>Высокий</option>
-                    </select>
-                <label>
- 
-                <div class="card__row">
-                    <button class="remove">Удалить</button>
-                    <button class="change">Изменить</button>
-                </div>
-                
-            </div>
-        `
-    })
-
-    toDo.innerHTML=html;
-}
-
-
-
-
-
-
-
+    
