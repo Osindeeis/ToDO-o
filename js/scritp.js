0f69bf1e7c944ae7eaf8dc43fd044f548865ac34
Vadim Sannikov <vadik8032@gmail.com>
2022-06-19 12:57:27 +0400

    Rewriting css

--- js/scritp.js
+++ js/scritp.js
@@ -10,7 +10,6 @@ let Select = document.querySelector("#priority");
 let CloseForm = document.querySelector("close-button")
 let cards;
 
-
 if(localStorage.card){
     cards = JSON.parse(localStorage.getItem("card"))
 }
@@ -44,16 +43,11 @@ const CreateCard = (card, index) => {
         <hr/>
         <p class="scroll">${card.description}</p>
         <label>Приоритет:
-            <select>
-                <option>${card.Select}</option>
-            </select>
-        <label>
-
+            <p class="priority">${card.Select}</p>
+        </label>
         <div class="card__row">
             <button onClick='removeLocal(${index})' class="remove">Удалить</button>
-            <button class="change">Изменить</button>
         </div>
-    
     </div>
     `
 }
@@ -65,12 +59,10 @@ const removeLocal = (index)=>{
         updateLocal();
         Show();
     } 
-    
 }
 
 const Show = ()=>{
-    toDo.innerHTML=""
-    
+    toDo.innerHTML="" 
     if(cards.length>0){
         FilterCard()
         cards.forEach((item,index) => {
@@ -85,14 +77,12 @@ const updateLocal = ()=>{
     localStorage.setItem("card",JSON.stringify(cards))
 }
 
-
 addCard.addEventListener("click",()=>{
     cards.push(new Card(Inputtitle.value,InputText.value, Select.value))
     updateLocal();
     Show();
     turnOffDisplay();
 })
-
 
 const turnOffDisplay = ()=>{
     form.style.opacity = 0;
